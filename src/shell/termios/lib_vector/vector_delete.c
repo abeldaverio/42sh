7f18c93e03700ac3c8b98caa7075f8c68e5e592b
Remnou2 <remi.nouveau@epitech.eu>
2024-05-08 16:39:18 +0200

    fix: now puts end of line when deleting

--- src/shell/termios/lib_vector/vector_delete.c
+++ src/shell/termios/lib_vector/vector_delete.c
@@ -11,6 +11,7 @@
 void vector_delete(void *data, size_t index)
 {
     vector_t *vector = (vector_t *)(*(void **)data - sizeof(vector_t));
+    char end = '\0';
 
     if (index > vector->current)
         return;
@@ -18,5 +19,8 @@ void vector_delete(void *data, size_t index)
         memmove(*(void **) data + (index * vector->size), (*(void **) data) +
             ((index + 1) * vector->size), vector->size *
             (vector->current) - index);
+    if (index + 1 == vector->current)
+        memcpy((*(void **) data) + ((index) * vector->size), &end,
+            vector->size);
     vector->current--;
 }
